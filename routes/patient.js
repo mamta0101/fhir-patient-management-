c24fc8d5f181ca759bd628a1ddd6582d1d1fdeef
Mamta-bisht0522 <mamtabisht.0522@gmail.com>
2023-10-22 11:48:18 +0530

    Update : auth in patient apis

--- routes/patient.js
+++ routes/patient.js
@@ -2,10 +2,10 @@ const router = require('express').Router()
 const { patientController } = require('../controllers/patient')
 const auth = require('../middlewares/auth')
 
-router.post('/add_patient', patientController.addPatient)
+router.post('/add_patient',auth, patientController.addPatient)
 router.get('/all_patients', patientController.getAllPatients)
 router.get('/patient/:fhirId', patientController.getPatients)
-router.put('/update_patient/:fhirId', patientController.updatePatient)
-router.delete('/del_patient/:fhirId', patientController.deletepatient)
+router.put('/update_patient/:fhirId',auth, patientController.updatePatient)
+router.delete('/del_patient/:fhirId', auth,patientController.deletepatient)
 
 module.exports = router
