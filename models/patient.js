199a33877eae4ed9461ba79bb2e1e17ff6716a15
Mamta-bisht0522 <mamtabisht.0522@gmail.com>
2023-10-22 22:06:01 +0530

    update : addpatient api

--- models/patient.js
+++ models/patient.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const patientSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    dateOfBirth: { type: Date, required: true },
+    dateOfBirth: { type: String, default : ""},
     gender: { type: String, required: true },
     fhirResponse: { type: Object },
     createdBy: {
